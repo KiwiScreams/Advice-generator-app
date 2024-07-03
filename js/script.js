16797179d50fa4dc2dd439c95a45e3bc44601b02
KiwiScreams <screamskiwi3@gmail.com>
2024-07-03 21:59:23 +0400

    full project

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const advice_text = document.getElementById('advice-text');
+const advice_number = document.getElementById('advice-number');
+const dicee_btn = document.getElementById('btn');
+const getPosts = async () => 
+{
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    console.log(data);
+    advice_number.innerHTML =  "#" + data.slip.id;
+    advice_text.innerHTML = '"' + data.slip.advice + '"';
+}
+dicee_btn.addEventListener('click', function()
+{
+    getPosts();
+});
+getPosts();
